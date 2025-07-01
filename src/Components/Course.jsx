5a26175bf1d4d06c3c44120a22561776164bb9fb
Xavorin <bossespro53@gmail.com>
2025-07-01 14:34:46 +0600

    Keys added to list elements

--- src/Components/Course.jsx
+++ src/Components/Course.jsx
@@ -15,7 +15,7 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <>
-      {props.parts.map((part) => <Part part={part.name} exercises={part.exercises}></Part>)}
+      {props.parts.map((part) => <Part key={part.id} part={part.name} exercises={part.exercises}></Part>)}
     </>
   )
 }
